a16eedc43e030ba4f1e8a8e356879a6684a9df35
Dell <Dell@DESKTOP-1674E11>
2022-10-14 10:14:41 +0530

    first commit

---
+++ gitProject/src/gitProject/FirstProg.java
@@ -0,0 +1,12 @@
+package gitProject;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("nagarpamma ");
+	}
+
+}
